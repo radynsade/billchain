02a58ad321e913f4052c06ee288b70436f46f561
Nikita Prokopenko <nikita.p@finassets.io>
2025-08-22 17:28:07 +0300

    woo

---
+++ address.go
@@ -0,0 +1,18 @@
+package main
+
+const (
+	Bech32BtcMainnetHrp = "bc"
+	Bech32BtcTestnetHrp = "tb"
+	Bech32BtcRegtestHrp = "bcrt"
+	Bech32LtcMainnetHrp = "ltc"
+	Bech32LtcTestnetHrp = "tltc"
+)
+
+// TODO: Add more base58 versions, ask AI for more.
+const (
+	Base58BtcMainnetVersion  = 0x00
+	Base58BtcMainnet2Version = 0x05
+	Base58BtcTestnetVersion  = 0x6F
+	Base58TronMainnetVersion = 0x41
+	Base58TronTestnetVersion = 0x49
+)
